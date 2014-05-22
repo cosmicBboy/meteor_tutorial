392803a3c8e69a53a0db43345feda88c18c7b0fc
nielsbantilan <niels.bantilan@gmail.com>
2014-05-22 18:21:24 -0400

    added edit posts form

--- microscope/lib/router.js
+++ microscope/lib/router.js
@@ -12,6 +12,11 @@ Router.map(function() {
         data: function() {return Posts.findOne(this.params._id); }
     });
 
+    this.route('postEdit', {
+        path: '/posts/:_id/edit',
+        data: function() { return Posts.findOne(this.params._id); }
+    });
+
     this.route('postSubmit', {
         path: '/submit'
     });
@@ -24,7 +29,7 @@ var requireLogin = function(pause) {
         else
             this.render('accessDenied');
 
-        pause();
+        this.stop();
     }
 }
 
