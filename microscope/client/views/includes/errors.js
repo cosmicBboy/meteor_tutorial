eee40c987ec52946d217f9ae9aa8a4cfb8698a65
nielsbantilan <niels.bantilan@gmail.com>
2014-05-22 22:06:07 -0400

    Monitor which errors have been seen, and clear on routing.

--- microscope/client/views/includes/errors.js
+++ microscope/client/views/includes/errors.js
@@ -3,3 +3,12 @@ Template.errors.helpers({
         return Errors.find();
     }
 });
+
+//set seen attribute of the error to be true, 
+//1 millisecond after error template is rendered
+Template.error.rendered = function() {
+    var error = this.data;
+    Meteor.defer(function() {
+        Errors.update(error._id, {$set: {seen: true}});
+    });
+};
