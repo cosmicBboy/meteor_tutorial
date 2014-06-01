6e23d043d27c2dc9e6a152f57621b63b835f9db3
nielsbantilan <niels.bantilan@gmail.com>
2014-06-01 11:31:26 -0400

    use a single post subscription to ensure that we can always see always see specific posts

--- microscope/server/publications.js
+++ microscope/server/publications.js
@@ -1,5 +1,9 @@
-Meteor.publish('posts', function(sort, limit){
-    return Posts.find({}, {sort: sort, limit: limit})
+Meteor.publish('posts', function(options){
+    return Posts.find({}, options)
+});
+
+Meteor.publish('singlePost', function(id) {
+    return id && Posts.find(id);
 });
 
 Meteor.publish('comments', function(postId){
@@ -9,3 +13,4 @@ Meteor.publish('comments', function(postId){
 Meteor.publish('notifications', function() {
     return Notifications.find({userId: this.userId});
 });
+
