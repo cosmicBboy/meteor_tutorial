9c76318cf10ddc0fb665388eca6d65fb4f1a9037
nielsbantilan <niels.bantilan@gmail.com>
2014-06-01 18:19:41 -0400

    Added routes for post lists, and pages to display them

--- microscope/client/views/posts/post_edit.js
+++ microscope/client/views/posts/post_edit.js
@@ -17,4 +17,13 @@ Template.postEdit.events({
             }
         });
     },
+
+    'click .delete': function(e) {
+        e.preventDefault();
+        if (confirm("Delete this post?")) {
+            var currentPostId = this._id;
+            Posts.remove(currentPostId);
+            Router.go('home');
+        }
+    }
 });
