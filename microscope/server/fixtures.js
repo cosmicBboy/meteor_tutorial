c702753a99a4a555cfe9edd5812d467ba755f989
nielsbantilan <niels.bantilan@gmail.com>
2014-06-01 20:41:23 -0400

    Added active classes to the header

--- microscope/server/fixtures.js
+++ microscope/server/fixtures.js
@@ -24,7 +24,8 @@ if (Posts.find().count() === 0) {
         author: sacha.profile.name,
         url: 'http://sachagreif.com/introducing-telescope/',
         submitted: now - 7 * 3600 * 1000,
-        commentsCount: 2
+        commentsCount: 2,
+        upvoters: [], votes: 0
     });
 
     Comments.insert({
@@ -57,7 +58,8 @@ if (Posts.find().count() === 0) {
         author: tom.profile.name,
         url: 'http://meteor.com',
         submitted: now - 10 * 3600 * 1000,
-        commentsCount: 0
+        commentsCount: 0,
+        upvoters: [], votes: 0
     });
 
     Posts.insert({
@@ -66,7 +68,8 @@ if (Posts.find().count() === 0) {
         author: tom.profile.name,
         url: 'http://themeteorbook.com',
         submitted: now - 12 * 3600 * 1000,
-        commentsCount: 0
+        commentsCount: 0,
+        upvoters: [], votes: 0
     });
 
     for (var i = 0; i < 10; i++) {
